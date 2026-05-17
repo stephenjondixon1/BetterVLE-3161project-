1af8aa8420b3fd3184b7d646f1a7fb252d15cf93
Victoria Whyte <vwhyte10@yahoo.com>
2026-05-17 13:01:48 -0500

    configuration for tidb cloud

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,12 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Database2016!',
+  host: process.env.DB_HOST,               
+  port: Number(process.env.DB_PORT) || 4000,  
+  user: process.env.DB_USER,               
+  password: process.env.DB_PASSWORD,      
   database: process.env.DB_NAME || 'betterVLE',
+  ssl: { rejectUnauthorized: true }, 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
