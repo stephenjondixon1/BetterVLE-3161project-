0fed96ef461a0a3ab53763e9646dddae850b446a
victoriawh <vwhyte10@yahoo.com>
2026-05-16 21:34:58 -0500

    Delete bettervle directory

--- bettervle/src/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`BetterVLE API listening on port ${PORT}`);
-});
